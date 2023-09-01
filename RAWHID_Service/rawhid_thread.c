d850ed11ec52cabd36daad5e2409caf874ea69f1
sungodra <avons394@gmail.com>
2023-08-31 23:20:27 -0500

    comment hid thread

--- RAWHID_Service/rawhid_thread.c
+++ RAWHID_Service/rawhid_thread.c
@@ -11,25 +11,31 @@ void send_ping(hid_device* handle);
  * @return 0 on successful execution, -1 on failure.
  */
 DWORD WINAPI rawhid_device_thread(LPVOID thread_config) {
-    int ret = 0; // Use to collect the return status
-    hid_device* handle = NULL;
-    unsigned char message[MESSAGE_SIZE_BITS] = { 0 };
+    int ret = 0; // Variable to store the return status
+    hid_device* handle = NULL; // Handle for the HID device
+    unsigned char message[MESSAGE_SIZE_BITS] = { 0 }; // Message buffer
 
+    // Cast thread_config to its proper type
     hid_thread_config* config = (hid_thread_config*)thread_config;
+
+    // Validate that configuration and device information isn't NULL
     if (!config || !config->device_info || !config->shared_data) {
         write_log(_ERROR, "Configuration or Device information is NULL.\n");
         ret = -1;
         goto cleanup;
     }
 
+    // Log Vendor ID and Product ID
     write_log_format(_INFO, "Vendor ID: 0x%x, Product ID: 0x%x", config->device_info->vendor_id, config->device_info->product_id);
 
+    // Initialize the HID API
     if (hid_init()) {
         write_log(_ERROR, "Unable to initialize HIDAPI.\n");
         ret = -1;
         goto cleanup;
     }
 
+    // Get and open the device handle
     handle = get_handle(config->device_info);
     open_usage_path(config->device_info, &handle);
     if (!handle) {
@@ -38,9 +44,13 @@ DWORD WINAPI rawhid_device_thread(LPVOID thread_config) {
         goto cleanup;
     }
 
+    // Set the device to non-blocking mode
     hid_set_nonblocking(handle, true);
+
+    // Get the shared data
     shared_thread_data* shared_data = config->shared_data;
 
+    // Main loop for reading from the device
     while (true) {
         int read_status = hid_read(handle, message, sizeof(message));
         if (read_status < 0) {
@@ -49,14 +59,15 @@ DWORD WINAPI rawhid_device_thread(LPVOID thread_config) {
             goto cleanup;
         }
 
+        // If read is successful
         if (read_status > 0) {
             write_log(_DEBUG, "hid read", message);
             set_message_to_tcp(shared_data, message, sizeof(message));
-            send_ping(handle);
+            send_ping(handle); // Send a ping message
         }
     }
 
-cleanup:
+cleanup: // Cleanup label for resource freeing and exit
     if (handle) {
         hid_close(handle);
     }
@@ -81,6 +92,7 @@ void send_ping(hid_device* handle) {
 
     // Prepare the ping message
     unsigned char ping[] = { 0x00, 0x01 };
+
     // Attempt to send the ping message
     if (write_to_handle(handle, ping, sizeof(ping)) < 0) {
         write_log_format(_ERROR, "Failed to send ping: %s", hid_error(handle));
